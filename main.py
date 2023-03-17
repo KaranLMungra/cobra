578e0b55ae28eb0cae87832bda7ad91bb68a2936
KaranLMungra <klmungra23@gmail.com>
2023-03-17 22:10:54 +0530

    Just improved code quality!

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from dbg import Dbg
 
 
 @Dbg
-def pow(x: int, y: int, **kwargs) -> int:
+def foo(x: int, y: int, **kwargs) -> int:
     _ = kwargs
     result = x ** y
     return result
 
 
 if __name__ == '__main__':
-    print(pow(pow(pow(2, 2, a='2'), 3), 5))
+    print(foo(foo(foo(2, 2, a='2'), 3), 5))
