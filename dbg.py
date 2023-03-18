7c48b1f1e1e26abceff34a1cb498bb6ef24fad69
KaranLMungra <klmungra23@gmail.com>
2023-03-18 05:34:10 +0530

    Refactored print statements and added support for true color checking.

--- dbg.py
+++ dbg.py
@@ -1,5 +1,7 @@
 from typing import Callable
 import inspect
+import sys
+import os
 
 
 def __rgb(r: int = 255, b: int = 255, g: int = 255, mode=False):
@@ -18,14 +20,37 @@ __MONOKAI_COLOR_PALLETE = {
     'Blue': __rgb(108, 153, 187),  # 6c99bb
 }
 
+kwargs_output = ', {kwarg} = {}{kwarg_value}{}'
+args_output = '{arg} = {}{value}{}'
+dbg_output ='{}[{filename}]{}({lineno}){}{name}{}({args}{kwargs})->{}{result}{}'
+def supports_color(color: str) -> str:
+    """
+    Returns True if the running system's terminal supports color, and False
+    otherwise.
+    """
+    plat = sys.platform
+    supported_platform = plat != 'Pocket PC' and (plat != 'win32' or
+                                                  'ANSICON' in os.environ)
+    is_a_tty = hasattr(sys.stdout, 'isatty') and sys.stdout.isatty()
+    return color if supported_platform and is_a_tty else ''
+
 
 def __kwargs_str(**kwargs) -> str:
-    return ''.join(map(lambda kwarg: f', {kwarg[0]} = {__MONOKAI_COLOR_PALLETE["Green"]}{kwarg[1].__repr__()}{__MONOKAI_COLOR_PALLETE["White"]}', kwargs.items()))
+    return ''.join(map(lambda x:
+                       kwargs_output.format(supports_color(__MONOKAI_COLOR_PALLETE["Green"]),
+                                            supports_color(__MONOKAI_COLOR_PALLETE["White"]), kwarg=x[0],
+                                                      kwarg_value=x[1].__repr__()), kwargs.items()))
 
 
 def __args_str(args: list[str], *values) -> str:
-    return ''.join(map(lambda arg: f', {arg[1]} = {__MONOKAI_COLOR_PALLETE["Green"]}{values[arg[0]].__repr__()}{__MONOKAI_COLOR_PALLETE["White"]}' if arg[0] else
-                       f'{arg[1]} = {__MONOKAI_COLOR_PALLETE["Green"]}{values[arg[0]]}{__MONOKAI_COLOR_PALLETE["White"]}', enumerate(args)))
+    return ''.join(map(lambda arg: ', ' +
+                       args_output.format(__MONOKAI_COLOR_PALLETE["Blue"],
+                                          __MONOKAI_COLOR_PALLETE["White"], arg=arg[1],
+                                                      value=values[arg[0]]) if arg[0] else
+                       args_output.format(__MONOKAI_COLOR_PALLETE["Blue"],
+                                         __MONOKAI_COLOR_PALLETE["White"],
+                                          arg=arg[1], value=values[arg[0]]),
+                       enumerate(args)))
 
 
 def Dbg(f: Callable):
@@ -34,7 +59,16 @@ def Dbg(f: Callable):
     def wrapper(*args, **kwargs):
         stack = inspect.stack()
         result = f(*args, **kwargs)
-        print(
-            f'{__MONOKAI_COLOR_PALLETE["Yellow"]}[{stack[1].filename}]{__MONOKAI_COLOR_PALLETE["Pink"]}({stack[1].lineno}){__MONOKAI_COLOR_PALLETE["Blue"]}{f.__qualname__}{__MONOKAI_COLOR_PALLETE["White"]}({__args_str(fnargs,*args)}{__kwargs_str(**kwargs)})-> {__MONOKAI_COLOR_PALLETE["Green"]}{result.__repr__()}{__MONOKAI_COLOR_PALLETE["White"]}')
+        print(dbg_output.format(__MONOKAI_COLOR_PALLETE['Yellow'],
+                                 __MONOKAI_COLOR_PALLETE['Pink'],
+                                 __MONOKAI_COLOR_PALLETE['Blue'],
+                                 __MONOKAI_COLOR_PALLETE['White'],
+                                 __MONOKAI_COLOR_PALLETE['Green'],
+                                 __MONOKAI_COLOR_PALLETE['White'],
+                                 filename=stack[1].filename,
+                                 lineno=stack[1].lineno, name=f.__qualname__,
+                                 args=__args_str(fnargs, *args),
+                                 kwargs=__kwargs_str(**kwargs),
+                                result=result.__repr__()))
         return result
     return wrapper
